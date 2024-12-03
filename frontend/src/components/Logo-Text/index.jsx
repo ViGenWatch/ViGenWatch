fdfa3719c73373baa229cd29be4ca09031419610
khaitd0452 <tranduckhai26112003@gmail.com>
2024-12-03 16:20:15 +0700

    UI fix color logo

--- frontend/src/components/Logo-Text/index.jsx
+++ frontend/src/components/Logo-Text/index.jsx
@@ -8,15 +8,16 @@ const cx = classNames.bind(style);
 const LogoText = ({ style }) => {
   return (
     <div style={style} className={cx('icon-group__text')}>
-      <span className={cx('icon-group__text-n')}>N</span>
+      <span className={cx('icon-group__text-v')}>V</span>
+      <span className={cx('icon-group__text-i')}>i</span>
+      <span className={cx('icon-group__text-g')}>G</span>
       <span className={cx('icon-group__text-e')}>e</span>
-      <span className={cx('icon-group__text-x')}>x</span>
+      <span className={cx('icon-group__text-n')}>n</span>
+      <span className={cx('icon-group__text-w')}>W</span>
+      <span className={cx('icon-group__text-a')}>a</span>
       <span className={cx('icon-group__text-t')}>t</span>
-      <span className={cx('icon-group__text-p')}>P</span>
+      <span className={cx('icon-group__text-c')}>c</span>
       <span className={cx('icon-group__text-h')}>h</span>
-      <span className={cx('icon-group__text-y')}>y</span>
-      <span className={cx('icon-group__text-l')}>l</span>
-      <span className={cx('icon-group__text-o')}>o</span>
     </div>
   );
 };
